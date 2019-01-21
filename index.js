d4a6d753fde8bc52e5863d35a86ffad1be2ef0f3
Antoni Kepinski <keppi@o2.pl>
2019-01-21 21:45:37 +0100

    small improvements

--- index.js
+++ index.js
@@ -24,85 +24,94 @@ client.on('guildMemberAdd', async member => {
 	if (!channel) {
 		return;
 	}
+
 	channel.send(`Witaj na serwerze, ${member}!`);
 });
 
 // Commands
 client.on('message', async message => {
-	if (message.content.substring(0, config.prefix.length) === config.prefix) {
-		const command = message.content.slice(config.prefix.length);
+	if (message.author.bot) {
+		return;
+	}
 
-		// Show help message
-		if (command === 'pomoc') {
-			try {
-				const embed = {
-					title: 'Dostępne komendy:',
-					color: 0x01579B,
-					description: `
+	if (message.content.indexOf(config.prefix) !== 0) {
+		return;
+	}
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	// Show help message
+	if (command === 'pomoc') {
+		try {
+			const embed = {
+				title: 'Dostępne komendy:',
+				color: 0x01579B,
+				description: `
 !pomoc - wyświetla wiadomość pomocniczą
 !linki - wyświetla listę przydatnych linków
 !changelog - wyświetla opis zmian dokonanych w najnowszej wersji Electrona
-!ping - podaje ping użytkownika
+!ping - podaje ping serwera
          `,
-					footer: {
-						icon_url: 'https://imgur.com/I04By3T.png',
-						text: 'Bot rozwijany jest przez Linuxa'
-					}
-				};
-				await message.channel.send({embed});
-			} catch (error) {
-				console.log(error);
-			}
+				footer: {
+					icon_url: 'https://imgur.com/I04By3T.png',
+					text: 'Bot rozwijany jest przez Linuxa'
+				}
+			};
+			await message.channel.send({embed});
+		} catch (error) {
+			console.log(error);
 		}
+	}
 
-		// Show useful links related to the Electron Framework
-		if (command === 'linki') {
-			try {
-				const embed = {
-					title: 'Przydatne linki:',
-					color: 0x01579B,
-					description: `
+	// Show useful links related to the Electron Framework
+	if (command === 'linki') {
+		try {
+			const embed = {
+				title: 'Przydatne linki:',
+				color: 0x01579B,
+				description: `
 - ${'[Strona społeczności Electron Polska](https://electronpl.github.io)'}
 - ${'[Główna strona projektu Electron](https://electronjs.org/)'}
 - ${'[Dokumentacja projektu Electron](https://electronjs.org/docs)'}
 - ${'[Ciekawe zasoby związane z projektem Electron](https://github.com/sindresorhus/awesome-electron)'}
 - ${'[Strona do tłumaczenia dokumentacji projektu na Język polski](https://crowdin.com/project/electron/pl#)'}
          `,
-					footer: {
-						icon_url: 'https://imgur.com/I04By3T.png',
-						text: 'Bot rozwijany jest przez Linuxa'
-					}
-				};
-				await message.channel.send({embed});
-			} catch (error) {
-				console.log(error);
-			}
+				footer: {
+					icon_url: 'https://imgur.com/I04By3T.png',
+					text: 'Bot rozwijany jest przez Linuxa'
+				}
+			};
+			await message.channel.send({embed});
+		} catch (error) {
+			console.log(error);
 		}
+	}
 
-		// Check user's ping
-		if (command === 'ping') {
-			try {
-				const {ping} = client;
-				const embed = {
-					title: 'Pong!',
-					color: 0x01579B,
-					description: `Twój ping wynosi około: ${'`' + round(ping, 0) + 'ms' + '`'}`,
-					footer: {
-						icon_url: 'https://imgur.com/I04By3T.png',
-						text: 'Bot rozwijany jest przez Linuxa'
-					}
-				};
-				await message.channel.send({embed});
-			} catch (error) {
-				console.log(error);
-			}
+	// Check user's ping
+	if (command === 'ping') {
+		try {
+			const {ping} = client;
+			const embed = {
+				title: 'Pong!',
+				color: 0x01579B,
+				description: `Czas odpowiedzi serwera wyniósł ${'`' + round(ping, 0) + 'ms' + '`'}`,
+				footer: {
+					icon_url: 'https://imgur.com/I04By3T.png',
+					text: 'Bot rozwijany jest przez Linuxa'
+				}
+			};
+			await message.channel.send({embed});
+		} catch (error) {
+			console.log(error);
 		}
+	}
 
-		// Get changelog from the latest Electron version
-		if (command === 'changelog') {
-			try {
-				const version = await latestVersion('electron');
-				const info = await github.query(`
+	// Get changelog from the latest Electron version
+	if (command === 'changelog') {
+		try {
+			const version = await latestVersion('electron');
+			const info = await github.query(`
 				{
 					repository(owner: "electron", name: "electron") {
 					  release(tagName: "v${version}") {
@@ -111,21 +120,26 @@ client.on('message', async message => {
 					}
 				  }				  
 				`);
-				const embed = {
-					title: `Electron ${version}`,
-					color: 0x01579B,
-					description: `
+
+			const releaseDescription = () => {
+				console.log();
+			};
+
+			releaseDescription();
+			const embed = {
+				title: `Electron ${version}`,
+				color: 0x01579B,
+				description: `
 					${'```markdown\n' + info.repository.release.description + '\n' + '```'}
 					`,
-					footer: {
-						icon_url: 'https://imgur.com/I04By3T.png',
-						text: 'Bot rozwijany jest przez Linuxa'
-					}
-				};
-				await message.channel.send({embed});
-			} catch (error) {
-				console.log(error);
-			}
+				footer: {
+					icon_url: 'https://imgur.com/I04By3T.png',
+					text: 'Bot rozwijany jest przez Linuxa'
+				}
+			};
+			await message.channel.send({embed});
+		} catch (error) {
+			console.log(error);
 		}
 	}
 });
